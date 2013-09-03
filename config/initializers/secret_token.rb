5988d79933456714d88700af952d9abf7ae2716f
Charles Denninger <charles@desk.com>
2013-09-03 16:16:12 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QAENV::Application.config.secret_token = '291613c8fc982eb4614bb5dfc6d2a2cc5a65a23f2d46611c37412b8548268e86299ce804a4f52b431f36f62e8e0a8da662fc900d29cae032bc3741fecec312c5'
